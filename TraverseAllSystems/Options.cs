b5d99ef3f332b23b0f7f559778ad94c0a6b3e727
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-06-23 14:28:11 -0700

    implemented Options class

---
+++ TraverseAllSystems/Options.cs
@@ -0,0 +1,19 @@
+﻿namespace TraverseAllSystems
+{
+  class Options
+  {
+    /// <summary>
+    /// Store element id or UniqueId in JSON output?
+    /// </summary>
+    public const bool StoreUniqueId = false;
+    public const bool StoreElementId = !StoreUniqueId;
+
+    /// <summary>
+    /// Store parent node id in child, or recursive 
+    /// tree of children in parent?
+    /// </summary>
+    public const bool StoreParentInChildNode = false;
+    public const bool StoreChildrenInParentNode 
+      = !StoreParentInChildNode;
+  }
+}
